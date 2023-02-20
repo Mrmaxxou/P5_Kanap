7a4cb344c2c9648263d10805670f5273e0f15556
Mrmaxxou <openrougeyron@gmail.com>
2023-02-20 12:48:03 +0100

    Intégration des produits sur la page produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,70 @@
 //Récupération de la chaîne de requête dans l'URL
-const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
+const productIdUrl = window.location.search;
+console.log(productIdUrl);
 
 // Extraction de l'id 
-let urlSearchparams = new URLSearchParams(queryString_url_id);
-console.log(urlSearchparams);
+let productId = new URLSearchParams(productIdUrl);
+console.log(productId);
 
-const id = urlSearchparams.get('_id');
+const id = productId.get('_id');
 console.log(id);
 
+//Création de l'URL de recherche dans l'API
+const requete = new XMLHttpRequest()
+const methodeHttp= "GET"
+const url = "http://localhost:3000/api/products/"+id
+console.log(url)
+
+// Recherche des informations avec L'URL lié à L'Id dans l'API
+
+fetch (url)
+// Après avoir reçu la réponse donne le résultat en json
+   .then(response => response.json())
+// Le résultat reçu traité en json sera appelé Produits
+   .then((productIdData) => {
+    // Montre moi dans la console les données récupérées sous forme de tableau.
+    console.log(productIdData);
+    // Appel de la fontion d'affichage des produits
+    loadData(productIdData);
+   })
+// Si il y a une erreur envoi l'erreur en console
+.catch((error) => {
+   console.log(error);
+});
+
+
+// Fonction affichage des données présente dans l'API avec l'id du produit
+
+function loadData(productIdData) {
+   if (productIdData != null) {
+      let 
+      data = document.querySelector('.item__img');
+      data.innerHTML += `<img src="${productIdData.imageUrl}" alt="${productIdData.altTxt}">`;
+
+      data = document.querySelector('#title');
+      data.textContent = productIdData.name;
+
+      data = document.querySelector('#price');
+      data.textContent = productIdData.price;
+
+      data = document.querySelector('#description');
+      data.textContent = productIdData.description;
+
+
+      for (let i=0; i < productIdData.colors.length; i++) {
+         colorsChoice(productIdData.colors[i]);
+
+   }
+}
+}
+
+// Fonction création des couleurs disponible pour la balise option
+
+function colorsChoice(colorChoice) {
+   const colorOption = document.createElement('option');
+   colorOption.value = colorChoice;
+   colorOption.textContent = colorChoice;
+   const parent = document.querySelector('#colors');
+   parent.appendChild(colorOption);
+}
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,30 +1,25 @@
 // Récupération des données de l'api 
-
 fetch ("http://localhost:3000/api/products")
 // Après avoir reçu la réponse donne le résultat en json
    .then(response => response.json())
-// Le résultat reçu traité en json sera appelé Produits
+// Le résultat reçu traité en json sera appelé produits
    .then((produits) => {
     // Montre moi dans la console les données récupérées sous forme de tableau.
     console.log(produits);
     // Appel de la fontion d'affichage des produits
-    kanaps(produits);
+    loadProduits(produits);
    })
-
-// Si il y a une erreur remplace le titre h1 par le contenu erreur 404 et renvoi l'erreur en console
-
-.catch((error) => {
-    document.querySelector(".titles").innerHTML = "Error 404";
+   // Si il y a une erreur renvoi l'erreur en console et viens remplacer le h1 par error 404
+   .catch((error) => {
+    document.querySelector(".titles").innerHTML = "<h1>Error 404</h1>";
     console.log("error 404");
 });
 
-
-
-function kanaps(index) {
+function loadProduits(produits) {
     // Déclaration de la variable de la zone article
     let zoneArticle = document.querySelector("#items");
-    // Boucle pour chaque indice appelé artcile dans l'index
-    for (let article of index){
+    // Boucle pour chaque indice appelé article dans l'index
+    for (let article of produits){
         // Création et ajout de l'article 
         zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
             <article>
